4a1fd0eb942a3d81cb7c7b08cc062dd1558fed22
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-09 15:14:12 -0600

    insert three paragraphs. Add button to turn futurama paragraph blue.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+		<script src="scripts.js"></script>
+		<title>Literary Mutilator</title>
+	</head>
+	<body>
+		<h1>Radom Paragraphs</h1>
+		<p id="futurama">Then throw her in the laundry room, which will hereafter be referred to as “the brig”. Kids have names? Come,
+		Comrade Bender! We must take to the streets! Shut up and get to the point! Have you ever tried just turning off the
+		TV, sitting down with your children, and hitting them?</p>
+		<p><button type="submit" onclick="futuramaBlue()">Turn This Paragraph Blue</button></p>
+
+		<p>Miata ipsum dolor amet Tom Matano 10ae dragon dipstick rev9 shocks. Ls shifter apex swaybar. Top down torsen touring
+		shifter. Ls barndoor se g-force montego, eyeball vents daisies laguna latch. Speeding zoom turbo, montego 10ae zoom
+		torsen mx-5 pop-up swaybar springs Nardi turbo nc. Sunburst din top up club, slow car fast shocks barn door mariner
+		shifter emerald top down miata. Sport Mazda abs laguna cas speeding zoom turbo.</p>
+		<p>The only girl I've ever loved	was born with roses in her eyes but then they buried her alive	one evening 1945
+		with just her sister at her side	And only weeks before the guns all came and rained on everyone	Now she's a
+		little boy in Spain playing pianos filled with flames on empty rings around the sun all sing to say my dream has come</p>
+	</body>
+</html>
