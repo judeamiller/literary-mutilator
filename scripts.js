0b72ed67757a000e8300375c21fe2cd96d0ca773
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-09 21:38:09 -0600

    add find and replace functions.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,22 @@ function futuramaBlue() {
 	var futuramaPagraph = document.getElementById("futurama");
 	futuramaPagraph.style.color = "blue";
 }
+
+function textReplace() {
+	let text = document.getElementById("holland");
+	document.body.innerHTML = document.body.innerHTML.replace ('girl' , 'CAPTAIN');
+	console.log(this);
+}
+
+function textReplace2() {
+	let text = document.getElementById("holland");
+	document.body.innerHTML = document.body.innerHTML.replace ('1945' , 'IN THE YEAR 2000');
+	console.log(this);
+}
+
+
+
+function rot13(string) {
+	var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+}
