199d1c8c78be7837e109f5024bd843984b9b4f7d
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-10 13:34:40 -0600

    add headings and format buttons

--- index.php
+++ index.php
@@ -19,33 +19,39 @@
 		<!--	Scripts Link	-->
 		<script src="scripts.js"></script>
 
+		<!--	Page Title	-->
 		<title>Literary Mutilator</title>
 	</head>
 	<body>
-		<h1>Radom Paragraphs</h1>
+		<div class="container">
+		<h1 class="text-center">Radom Paragraphs</h1>
+		<h3>This Paragraph Can Change Color</h3>
 		<p id="futurama">Then throw her in the laundry room, which will hereafter be referred to as “the brig”. Kids have names? Come,
 		Comrade Bender! We must take to the streets! Shut up and get to the point! Have you ever tried just turning off the
 		TV, sitting down with your children, and hitting them?</p>
-		<p><button type="submit" onclick="futuramaBlue()">Turn This Paragraph Blue</button></p>
+		<p><button class="btn btn-primary" type="submit" onclick="futuramaBlue()">Turn This Paragraph Blue</button></p>
 
+		<h3>Make nonsense even more nonsensical with ROT13</h3>
 		<p>Miata ipsum dolor amet Tom Matano 10ae dragon dipstick rev9 shocks. Ls shifter apex swaybar. Top down torsen touring
 		shifter. Ls barndoor se g-force montego, eyeball vents daisies laguna latch. Speeding zoom turbo, montego 10ae zoom
 		torsen mx-5 pop-up swaybar springs Nardi turbo nc. Sunburst din top up club, slow car fast shocks barn door mariner
 		shifter emerald top down miata. Sport Mazda abs laguna cas speeding zoom turbo.</p>
-		<p><button type="submit" id="rot13">Rot the text!</button></p>
+		<p><button class="btn btn-success" type="submit" id="rot13">Rot the text!</button></p>
 		<p>Rot(13)en Text: <span id="rotText"></span></p>
 
+		<h3>Ruin a classic lyric</h3>
 		<p id="holland">The only girl I've ever loved	was born with roses in her eyes but then they buried her alive	one evening 1945
 		with just her sister at her side	And only weeks before the guns all came and rained on everyone	Now she's a
 		little boy in Spain playing pianos filled with flames on empty rings around the sun all sing to say my dream has come</p>
 
-		<p><button type="submit" id="find and replace" onclick="textReplace(), textReplace2()">change it up</button></p>
+		<p><button class="btn btn-danger" type="submit" id="find and replace" onclick="textReplace(), textReplace2()">Ruin Lyrics</button></p>
 
 
 		<h4>Add More Text!</h4>
 		<p><button type="submit" id="addMoreText">ADD MORE TEXT</button> </p>
 
 		<p class="fillHere" onclick="populateParagraph()"></p>
+		</div>
 
 
 	</body>
